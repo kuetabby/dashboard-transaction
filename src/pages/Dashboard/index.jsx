1a8b9089dd26b351f7fc38528da777ddff560d48
kuetabby <vansmuhammad@gmail.com>
2022-02-16 03:17:00 +0700

    update layout to more readable

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -9,6 +9,27 @@ import styles from './index.less';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 const DashboardPage = () => {
+  const generateLayout = () => {
+    return [...Array(6).keys()].map((item) => {
+      const height = Math.ceil(Math.random() * 2) + 1;
+      return {
+        x: 0,
+        y: Math.floor(item / 2) * height,
+        w: 2,
+        h: height,
+      };
+    });
+  };
+
+  const layouts = { lg: generateLayout() };
+
+  const generateList = () => {
+    return layouts.lg.map((item, i) => {
+      const colors = ['red', 'green', 'blue', 'orange', 'yellow', 'pink'];
+      return <div key={i} style={{ backgroundColor: colors[i] }} />;
+    });
+  };
+
   return (
     <div>
       <div className={styles.title_wrapper}>
@@ -16,26 +37,16 @@ const DashboardPage = () => {
         <div className={styles.title}>Dashboard</div>
       </div>
       <Card bordered>
-        <ResponsiveReactGridLayout
-          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-          rowHeight={15}
-          className="layout"
-          useCSSTransforms
-        >
-          {[...Array(6).keys()].map((item) => {
-            const height = Math.ceil(Math.random() * 2) + 1;
-            const data_grid = {
-              x: 0,
-              y: Math.floor(item / 2) * height,
-              w: 2,
-              h: height,
-            };
-            const colors = ['red', 'green', 'blue', 'orange', 'yellow', 'pink'];
-            return (
-              <div key={item} data-grid={data_grid} style={{ backgroundColor: colors[item] }} />
-            );
-          })}
-        </ResponsiveReactGridLayout>
+        <div style={{ width: '100%' }}>
+          <ResponsiveReactGridLayout
+            layouts={layouts}
+            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+            rowHeight={15}
+            className="layout"
+          >
+            {generateList()}
+          </ResponsiveReactGridLayout>
+        </div>
       </Card>
     </div>
   );
