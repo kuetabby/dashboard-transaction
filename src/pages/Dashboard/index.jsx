a9c77432c2585e1e8843b977b73cd5159747ecb2
kuetabby <vansmuhammad@gmail.com>
2022-02-16 09:46:09 +0700

    fix this update didn't commited last night

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { Card } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
 
-import { Responsive, WidthProvider } from 'react-grid-layout';
+import RGL, { WidthProvider } from 'react-grid-layout';
 
 import styles from './index.less';
 
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
+const ResponsiveReactGridLayout = WidthProvider(RGL);
 
 const DashboardPage = () => {
   const generateLayout = () => {
-    return [...Array(6).keys()].map((item) => {
+    return [...Array(6).keys()].map((item, i) => {
       const height = Math.ceil(Math.random() * 2) + 1;
       return {
         x: 0,
         y: Math.floor(item / 2) * height,
         w: 2,
         h: height,
+        i: i.toString(),
       };
     });
   };
@@ -37,16 +38,16 @@ const DashboardPage = () => {
         <div className={styles.title}>Dashboard</div>
       </div>
       <Card bordered>
-        <div style={{ width: '100%' }}>
-          <ResponsiveReactGridLayout
-            layouts={layouts}
-            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-            rowHeight={15}
-            className="layout"
-          >
-            {generateList()}
-          </ResponsiveReactGridLayout>
-        </div>
+        <ResponsiveReactGridLayout
+          className="layout"
+          layouts={layouts}
+          cols={3}
+          // cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          // breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          rowHeight={30}
+        >
+          {generateList()}
+        </ResponsiveReactGridLayout>
       </Card>
     </div>
   );
